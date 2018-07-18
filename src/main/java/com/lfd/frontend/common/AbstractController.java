b3674b9811dfefe7fe77973181cf6bc909fb8a7e
Cao Yi <739822405@qq.com>
2018-07-18 23:23:31 +0800

    add common rowbounds method

--- src/main/java/com/lfd/frontend/common/AbstractController.java
+++ src/main/java/com/lfd/frontend/common/AbstractController.java
@@ -1,5 +1,8 @@
 package com.lfd.frontend.common;
 
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.web.bind.ServletRequestUtils;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -9,5 +12,13 @@ public class AbstractController {
     public String getLoginUserId(HttpServletRequest request) {
         final Object userId = request.getAttribute("userId");
         return userId == null ? null : (String) userId;
+    }
+
+    public RowBounds rowBounds(HttpServletRequest request) {
+        Integer size = ServletRequestUtils.getIntParameter(request, "size", 20);
+        final Integer page = ServletRequestUtils.getIntParameter(request, "page", 1);
+        size = size > 20 ? 20 : size;
+        final Integer offset = page == 0 ? page : (page - 1) * size;
+        return new RowBounds(offset, size);
     }
 }
